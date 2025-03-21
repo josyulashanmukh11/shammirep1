2b222b6d2371cb954908fb73f3788481f5876119
shanmukh <josyulashanmukh@gmail.com>
2025-03-21 20:10:51 +0530

    Added .gitignore for Terraform

---
+++ shannu.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws"{
+    region ="us-east-1"
+}
+
+
+resource  "aws_instance" "app_ec2" { 
+    ami = var.instance_ami
+    instance_type = "t2.micro"
+ tags = {
+    Name = "shanmukh"
+ }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-084568db4383264d4"
+}
